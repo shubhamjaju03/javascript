cc174345e7dbf781d69e1c9b986dad56669c4ff3
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-03 13:17:35 +0530

    Create operators.js to demonstrate JavaScript operators
    Add various operators demonstration in operators.js

---
+++ operators.js
@@ -0,0 +1,17 @@
+let a = 12
+let b = 13
+console.log(a + b) //addition operator
+console.log(a - b) //subtraction operator
+console.log(a * b) //multiplication operator
+console.log(a / b) //modulus operator gives the remainder of the division
+console.log(a % b) //modulus operator gives the remainder of the division
+console.log(a ** b) //exponentiation operator gives the power of a to b
+console.log(a == b) //equality operator checks if a is equal to b
+console.log(a != b) //inequality operator checks if a is not equal to b
+console.log(a > b) //greater than operator checks if a is greater than b
+console.log(a < b) //less than operator checks if a is less than b
+console.log(a >= b) //greater than or equal to operator checks if a is greater than or equal to b
+console.log(a <= b) //less than or equal to operator checks if a is less than or equal to b
+console.log(a === b) //strict equality operator checks if a is equal to b and also checks the type of a and b
+console.log(a !== b) //strict inequality operator checks if a is not equal to b and also checks the type of a and b
+console.log(a && b) //logical AND operator checks if both a and b are true
