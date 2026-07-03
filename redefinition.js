c991120058295aef63184a49a134e5cd955fd387
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-03 13:10:29 +0530

    Implement variable redefinition example in redefinition.js
    Added code demonstrating variable redefinition with 'var'.

---
+++ redefinition.js
@@ -0,0 +1,6 @@
+var a = 12
+console.log(a)
+var a = 13
+console.log(a)
+//re-declaring a variable with var is allowed, but it will overwrite the previous value. 
+//in simple words we call it redefinition. but with let and const it is not allowed. it will give an error.
