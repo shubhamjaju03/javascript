f3a0ece1b156104c7689a0ed2d1b14ca187674d0
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-03 13:02:36 +0530

    Add example of let, var, and const usage
    Demonstrate usage of let, var, and const in JavaScript.

---
+++ works of  let,var,const.js
@@ -0,0 +1,6 @@
+let a = 12 //with let you can change the value of a
+var a = 12 //with var you can change the value of a
+const b = 13 //with const you can not change the value of b
+console.log(a + b)
+b = 15
+console.log(a + b)
