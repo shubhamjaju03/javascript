c95fef0c72c1eb7df31709d2f7f22b3b2c4cfb4d
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-04 18:05:28 +0530

    Add mathematical functions and constants examples

---
+++ math function.js
@@ -0,0 +1,16 @@
+console.log(Math.floor(3.7)) //Math.floor() method rounds a number down to the nearest integer
+console.log(Math.ceil(3.7)) //Math.ceil() method rounds a number up to the nearest integer
+console.log(Math.round(3.7)) //Math.round() method rounds a number to the nearest integer 
+console.log(Math.max(3, 4, 5)) //Math.max() method returns the largest of zero or more numbers
+console.log(Math.min(3, 4, 5)) //Math.min() method returns the smallest of zero or more numbers
+console.log(Math.random()) //Math.random() method returns a random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.PI) //Math.PI property returns the value of PI
+console.log(Math.E) //Math.E property returns the value of E
+console.log(Math.sqrt(16)) //Math.sqrt() method returns the square root of a number
+console.log(Math.pow(2, 3)) //Math.pow() method returns the base to the exponent power, that is, base^exponent
+console.log(Math.abs(-5)) //Math.abs() method returns the absolute value of a number
+console.log(Math.sin(0)) //Math.sin() method returns the sine of a number
+console.log(Math.cos(0)) //Math.cos() method returns the cosine of a number
+console.log(Math.tan(0)) //Math.tan() method returns the tangent of a number
+console.log(Math.log(1)) //Math.log() method returns the natural logarithm (base E) of a number
+console.log(Math.log10(10)) //Math.log10() method returns the base 10 logarithm of a number
