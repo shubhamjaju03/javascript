12e87ef74765bbf880eadc51cdfcee313ab6c6a3
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-04 16:54:38 +0530

    Add for loop and for of loop examples
    Added examples of for loop and for of loop in JavaScript.

---
+++ for loop &for of.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+array = [1, 2, 3, 4, 5]  //array is a collection of elements
+for (const element of array) {  //for of loop is used to iterate over the elements of an array
+    console.log(element)    //it will print the elements of the array one by one
+} //for of loop is used to iterate over the properties of an object
