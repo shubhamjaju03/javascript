c6b27bd15e03e6047b48831d1c919d3747a8aed2
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-04 17:07:44 +0530

    Create string builtin.js

---
+++ string builtin.js
@@ -0,0 +1,7 @@
+let a = "Shubham"
+console.log(a.toUpperCase()) //toUpperCase() method converts the string to uppercase
+console.log(a.toLowerCase()) //toLowerCase() method converts the string to lowercase
+console.log(a.length) //length property returns the length of the string
+console.log(a.charAt(0)) //charAt() method returns the character at the specified index
+console.log(a.indexOf("h")) //indexOf() method returns the index of the first occurrence of the specified value
+console.log(a.lastIndexOf("h")) //lastIndexOf() method returns the index of the last occurrence of the specified value
