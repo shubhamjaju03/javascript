3b401baa938fd4eb46373716043b5e2a93254798
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2026-07-06 16:43:26 +0530

    Implement click event listeners for color changes
    Add click event listeners to elements with IDs a1, a2, and a3 to change their background colors.

---
+++ event_listener.js
@@ -0,0 +1,14 @@
+let x = document.querySelector("#a1");
+x.addEventListener("click", function() {
+    x.style.backgroundColor = "red";
+    x.style.color = "white";   
+});
+let y = document.querySelector("#a2");
+y.addEventListener("click", function() {
+    y.style.backgroundColor = "blue";
+});
+let z = document.querySelector("#a3");
+z.addEventListener("click", function() {
+    z.style.backgroundColor = "green";
+    z.style.color = "white";
+});
